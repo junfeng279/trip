680a576f960398fb1dfa93e3f511e4482151e208
junfeng279 <junfeng279@163.com>
2017-11-22 23:16:00 +0800

    增加springboot相关配置和常量配置

--- wantrip/src/main/java/com/wantrip/WantripApplication.java
+++ wantrip/src/main/java/com/wantrip/WantripApplication.java
@@ -1,8 +1,22 @@
 package com.wantrip;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 /**
  * 湾旅服务
  * Created by junfeng on 17-11-21.
  */
+@MapperScan("com.wantrip.dao")
+@SpringBootApplication
+@EnableEurekaClient
+@EnableTransactionManagement
 public class WantripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantripApplication.class, args);
+    }
 }
