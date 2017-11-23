cca4a9731b8b35c8a5f1c0f8ad7e7479eb2494a1
junfeng279 <junfeng279@163.com>
2017-11-23 22:21:00 +0800

    添加相关统计类

---
+++ wantrip/src/main/java/com/wantrip/dto/MetaDto.java
@@ -0,0 +1,17 @@
+package com.wantrip.dto;
+
+
+import com.wantrip.modal.vo.MetaVo;
+
+public class MetaDto extends MetaVo {
+
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
