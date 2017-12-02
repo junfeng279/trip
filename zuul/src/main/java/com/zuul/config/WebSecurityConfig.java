059cb864ed2d7c2461e4f0f5f69579a73b16bf13
junfeng279 <junfeng279@163.com>
2017-12-02 23:13:40 +0800

    前段模板权限配置

--- zuul/src/main/java/com/zuul/config/WebSecurityConfig.java
+++ zuul/src/main/java/com/zuul/config/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 //.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 // 对于获取token的rest api要允许匿名访问
                 .antMatchers("/auth/**").permitAll()
-                .antMatchers("/page/**").permitAll()
+                .antMatchers("/wantrip/page/**").permitAll()
                 .antMatchers("/hystrix.stream/**").permitAll()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated();
