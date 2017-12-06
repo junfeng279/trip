8d68eaa055217be5368f070ea9378f673907499f
junfeng279 <junfeng279@163.com>
2017-12-06 22:37:32 +0800

    后台登录页面js

---
+++ zuul/src/main/resources/static/admin/js/login.js
@@ -0,0 +1,52 @@
+/**
+ *后台登录页面js
+ */
+( function( window ) {
+    'use strict';
+    var login = new Vue({
+        el: "#login-form",
+        data: {
+            email: "",
+            password: "",
+            remember: false
+        },
+        methods: {
+            checked: function(data){
+                this.remember = !data;
+            },
+            submit: function(){
+                var that = this;
+                if(this.email==""){
+                    alert("用户名不能为空!");
+                    return ;
+                }
+                if(this.password==""){
+                    alert("密码不能为空!");
+                    return ;
+                }
+                var url = "/auth";
+                var params = {
+                    "username": that.email,
+                    "password": that.password,
+                    "remember": that.remember
+                };
+                $.ajax({
+                    url: url,
+                    type: "POST",
+                    dataType: "json",
+                    data: JSON.stringify(params),
+                    contentType: 'application/json',
+                    success: function(data){
+                        alert(data.token);
+                        debugger;
+                    },
+                    error: function(data){
+
+                    }
+                });
+
+
+            }
+        }
+    });
+})( window );
