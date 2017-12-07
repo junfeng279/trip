fed13349f2a6cf2b58d0bd3b5e0e0ed751cde807
junfeng279 <junfeng279@163.com>
2017-12-07 21:33:49 +0800

    后台页面header设置

---
+++ zuul/src/main/resources/static/admin/js/header.js
@@ -0,0 +1,31 @@
+
+var header = Vue.component('app-header', function(resolve, reject){
+    $.get("../../templates/admin/header.html").then(function(res){
+        resolve({
+            props: ['myMessage'],
+            template: res,
+            data: function(){
+                return {
+                    sidemenu: {
+                        home: "仪表盘",
+                        publish: "发布文章",
+                        article: "文章管理",
+                        page: "页面管理",
+                        comments: "评论管理",
+                        category: "分类/标签",
+                        attach: "文件管理",
+                        links: "链接管理",
+                        setting: "系统设置"
+                    },
+                    isActive: "home"
+                }
+            },
+            methods: {
+                menuclick: function(data){
+                    this.isActive = data;
+                    this.$emit("menuclick", data);
+                }
+            }
+        });
+    });
+});
